6d9e53df737773de0f2526129ef1938feee9a37b
Brian Oline <brian.oline@archoninfosys.com>
2014-11-12 11:54:31 -0600

    beginning to add new place picker component

---
+++ main.js
@@ -0,0 +1,25 @@
+﻿define(["knockout", "jquery", "./model",
+	"knockout.punches",
+	"template!./templates/index.html!place-picker-main",
+	"template!./templates/choice.html!place-picker-choice",
+], function (ko, $, ViewModel) {
+
+	return function () {
+		ko.punches.enableAll();
+		ko.punches.interpolationMarkup.enable();
+		ko.punches.attributeInterpolationMarkup.enable();
+
+		ko.bindingHandlers.placePicker = {
+			init: function (element) {
+				return {
+					controlsDescendantBindings: true
+				};
+			},
+			update: function (element, valueAccessor, allBindings) {
+				var unwrapped = ko.unwrap(valueAccessor());
+				ko.renderTemplate("place-picker-main", new ViewModel(unwrapped), null, element, "replaceChildren");
+			}
+		};
+		ko.virtualElements.allowedBindings.placePicker = true;
+	};
+});
---
+++ model.js
@@ -0,0 +1,20 @@
+﻿define(["knockout", "jquery", "data", "knockout.init", "lodash", "app/user/current", "knockout.integer"], function (ko, $, data, init, _, user) {
+	function ViewModel(modelData) {
+
+		this.placesUrl = urls.fips + "places/";
+		this.autoCompletePlacesUrl = placesUrl + "?q=%QUERY";
+		this.authToken = user.authToken;
+		this.templateName = "place-picker-choice";
+
+		this.fipsMappingFunction = function (data) {
+			var model = init(this, data);
+
+			return model;
+		}.bind(this);
+	}
+
+	return ViewModel;
+});
+
+
+
