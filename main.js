a02a89e3dc6394b6f803da9aebf5fb968b4a15df
Brian Oline <brian.oline@archoninfosys.com>
2014-11-12 14:03:00 -0600

    CS-12315: getting the palces typeahead to work

--- main.js
+++ main.js
@@ -3,23 +3,20 @@
 	"template!./templates/index.html!place-picker-main",
 	"template!./templates/choice.html!place-picker-choice",
 ], function (ko, $, ViewModel) {
+	ko.punches.enableAll();
+	ko.punches.interpolationMarkup.enable();
+	ko.punches.attributeInterpolationMarkup.enable();
 
-	return function () {
-		ko.punches.enableAll();
-		ko.punches.interpolationMarkup.enable();
-		ko.punches.attributeInterpolationMarkup.enable();
-
-		ko.bindingHandlers.placePicker = {
-			init: function (element) {
-				return {
-					controlsDescendantBindings: true
-				};
-			},
-			update: function (element, valueAccessor, allBindings) {
-				var unwrapped = ko.unwrap(valueAccessor());
-				ko.renderTemplate("place-picker-main", new ViewModel(unwrapped), null, element, "replaceChildren");
-			}
-		};
-		ko.virtualElements.allowedBindings.placePicker = true;
+	ko.bindingHandlers.placePicker = {
+		init: function (element) {
+			return {
+				controlsDescendantBindings: true
+			};
+		},
+		update: function (element, valueAccessor, allBindings) {
+			var unwrapped = ko.unwrap(valueAccessor());
+			ko.renderTemplate("place-picker-main", new ViewModel(unwrapped), null, element, "replaceChildren");
+		}
 	};
+	ko.virtualElements.allowedBindings.placePicker = true;
 });
--- model.js
+++ model.js
@@ -1,20 +1,22 @@
-﻿define(["knockout", "jquery", "data", "knockout.init", "lodash", "app/user/current", "knockout.integer"], function (ko, $, data, init, _, user) {
-	function ViewModel(modelData) {
+﻿define(["knockout", "app/urls", "jquery", "data", "knockout.init", "lodash", "app/user/current",
+	"knockout.integer"],
+	function (ko, urls, $, data, init, _, user) {
+		function ViewModel(modelData) {
 
-		this.placesUrl = urls.fips + "places/";
-		this.autoCompletePlacesUrl = placesUrl + "?q=%QUERY";
-		this.authToken = user.authToken;
-		this.templateName = "place-picker-choice";
+			this.placesUrl = urls.fips + "places/";
+			this.autoCompletePlacesUrl = this.placesUrl + "?q=%QUERY";
+			this.authToken = user.authToken;
+			this.templateName = "place-picker-choice";
 
-		this.fipsMappingFunction = function (data) {
-			var model = init(this, data);
+			this.mapping = function (data) {
+				var model = init(this, data);
 
-			return model;
-		}.bind(this);
-	}
+				return model;
+			}.bind(this);
+		}
 
-	return ViewModel;
-});
+		return ViewModel;
+	});
 
 
 
