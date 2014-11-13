2ea5caf96522412853e7f9efc0e139694db08735
Mark D'Arensbourg <mdarens@gmail.com>
2014-11-13 14:59:42 -0600

    Trigger place picker validation if nothing is picked CS-12315

--- main.js
+++ main.js
@@ -11,9 +11,13 @@
 		init: function (element, valueAccessor) {
 			var model = new ViewModel(valueAccessor());
 
-			$(element).on("typeahead:selected typeahead:autocompleted", function (e, suggestion) {
-				model.selected(suggestion);
-			});
+			$(element)
+				.on("focusout", function () {
+					model.selected.valueHasMutated();
+				})
+				.on("typeahead:selected typeahead:autocompleted", function (e, suggestion) {
+					model.selected(suggestion);
+				});
 
 			ko.renderTemplate("place-picker-main",model, null, element, "replaceChildren");
 
