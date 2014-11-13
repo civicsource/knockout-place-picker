2912d0ce657f9c408ea3d9b149c7fa1d4dc88239
Mark D'Arensbourg <mdarens@gmail.com>
2014-11-13 15:49:02 -0600

    Stop inlining click functions like a noob

--- model.js
+++ model.js
@@ -7,6 +7,9 @@
 			this.templateName = "place-picker-choice";
 
 			this.selected = ko.isObservable(selected) ? selected : ko.observable(selected);
+			this.selected.clear = function () {
+				this.selected(undefined);
+			}.bind(this);
 
 			this.mapping = function (data) {
 				var model = init(this, data);
